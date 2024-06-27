a29ec9b92d38ae7f2d27f5e17d7652d421f3604f
chulees4 <letrieuvan1203@gmail.com>
2024-06-27 10:17:25 +0700

    sync

--- src/main/java/com/mycompany/finaltermproject/LoginUI.java
+++ src/main/java/com/mycompany/finaltermproject/LoginUI.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.mycompany.finaltermproject;
-// Ga
+// Gadhsua
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
